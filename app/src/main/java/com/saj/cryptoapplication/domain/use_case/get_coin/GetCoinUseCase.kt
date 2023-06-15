f3ea470cfc5ffc1a55d33585f3209884a76fad1b
sajith <shamindasajith@gmail.com>
2023-06-16 00:05:26 +0530

    usecases added

--- app/src/main/java/com/saj/cryptoapplication/domain/use_case/get_coin/GetCoinUseCase.kt
+++ app/src/main/java/com/saj/cryptoapplication/domain/use_case/get_coin/GetCoinUseCase.kt
@@ -1,4 +1,28 @@
 package com.saj.cryptoapplication.domain.use_case.get_coin
 
-class GetCoinUseCase {
+import com.saj.cryptoapplication.common.Resource
+import com.saj.cryptoapplication.data.remote.dto.CoinDetailDto
+import com.saj.cryptoapplication.data.remote.dto.toCoinDetail
+import com.saj.cryptoapplication.domain.model.CoinDetail
+import com.saj.cryptoapplication.domain.repository.CoinRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import retrofit2.HttpException
+import java.io.IOException
+import javax.inject.Inject
+
+class GetCoinUseCase @Inject constructor(
+    private val repository: CoinRepository
+) {
+    operator fun invoke(coinId:String): Flow<Resource<CoinDetail>> = flow {
+        try {
+            emit(Resource.Loading())
+            val coin = repository.getCoinById(coinId = coinId).toCoinDetail()
+            emit(Resource.Success(coin))
+        } catch (e: HttpException) {
+            emit(Resource.Error(e.localizedMessage ?: "An unexpected error occured"))
+        } catch (e: IOException) {
+            emit(Resource.Error("Couldn't connect to the server"))
+        }
+    }
 }
--- app/src/main/java/com/saj/cryptoapplication/domain/use_case/get_coins/GetCoinsUseCase.kt
+++ app/src/main/java/com/saj/cryptoapplication/domain/use_case/get_coins/GetCoinsUseCase.kt
@@ -1,4 +1,27 @@
 package com.saj.cryptoapplication.domain.use_case.get_coins
 
-class GetCoinsUseCase {
+import com.saj.cryptoapplication.common.Resource
+import com.saj.cryptoapplication.data.remote.dto.toCoin
+import com.saj.cryptoapplication.domain.model.Coin
+import com.saj.cryptoapplication.domain.repository.CoinRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import retrofit2.HttpException
+import java.io.IOException
+import javax.inject.Inject
+
+class GetCoinsUseCase @Inject constructor(
+    private val repository: CoinRepository
+) {
+    operator fun invoke(): Flow<Resource<List<Coin>>> = flow {
+        try {
+            emit(Resource.Loading())
+            val coins = repository.getCoins().map { it.toCoin() }
+            emit(Resource.Success(coins))
+        } catch (e: HttpException) {
+            emit(Resource.Error(e.localizedMessage ?: "An unexpected error occured"))
+        } catch (e: IOException) {
+            emit (Resource.Error("Couldn't connect to the server"))
+        }
+    }
 }
